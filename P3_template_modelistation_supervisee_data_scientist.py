ca2db4e24768f2d98540dbd58c1658e3ccdac2ca
Emilie2393 <emilie.rich4rd@gmail.com>
2025-12-30 09:28:14 +0100

    P3 refactored

--- P3_template_modelistation_supervisee_data_scientist.py
+++ P3_template_modelistation_supervisee_data_scientist.py
@@ -11,186 +11,250 @@ import seaborn as sns
 import os 
 import numpy as np
 
-# ### Analyse Exploratoire
+class BuildingEnergyStudy():
 
-building_consumption = pd.read_csv("2016_Building_Energy_Benchmarking.csv")
+    def __init__(self):
+        self.target = "SiteEUI(kBtu/sf)"
+        self.df = pd.read_csv("2016_Building_Energy_Benchmarking.csv")
+        self.df_filtered = None
 
-# On regarde comment un batiment est défini dans ce jeu de données 
-building_consumption.head()
+    # ### Analyse Exploratoire
 
-# On regarde le nombre de valeurs manquantes par colonne ainsi que leur type 
-building_consumption.info()
+    def doc_analysis(self):
+        # On regarde comment un batiment est défini dans ce jeu de données 
+        self.df.head()
 
-# #### TERMINER L'ANALYSE EXPLORATOIRE 
+        # On regarde le nombre de valeurs manquantes par colonne ainsi que leur type 
+        self.df.info()
 
-# A réaliser : 
-# - Une analyse descriptive des données, y compris une explication du sens des colonnes gardées, des arguments derrière la suppression de lignes ou de colonnes, des statistiques descriptives et des visualisations pertinentes.
+        # #### TERMINER L'ANALYSE EXPLORATOIRE 
 
-# Qelques pistes d'analyse : 
+        # A réaliser : 
+        # - Une analyse descriptive des données, y compris une explication du sens des colonnes gardées, des arguments derrière la suppression de lignes ou de colonnes, des statistiques descriptives et des visualisations pertinentes.
 
-# Suppression des lignes concernant des immeubles d'habitation
-to_delete = ["Multifamily LR (1-4)", "Multifamily MR (5-9)", "Multifamily HR (10+)"]
-df = building_consumption[~building_consumption["BuildingType"].isin(to_delete)]
-df = df.drop(columns=["City", "State", "DataYear", "Latitude", "Longitude", "Comments", "DefaultData"]).copy()
+        # Qelques pistes d'analyse : 
 
-# * Identifier les colonnes avec une majorité de valeurs manquantes ou constantes en utilisant la méthode value_counts() de Pandas
-for column in df.columns:
-    print(f"\n--- {column} ---")
-    print(df[column].value_counts(normalize=True, dropna=False) * 100)
+        # Suppression des lignes concernant des immeubles d'habitation
+        to_delete = ["Multifamily LR (1-4)", "Multifamily MR (5-9)", "Multifamily HR (10+)"]
+        df = self.df[~self.df["BuildingType"].isin(to_delete)]
+        self.df_filtered = df.drop(columns=["City", "State", "DataYear", "Latitude", "Longitude", "Comments", "DefaultData"]).copy()
 
-print(f"\n---**** {df["LargestPropertyUseType"].value_counts(normalize=True, dropna=False) * 100} ---")
+        # * Identifier les colonnes avec une majorité de valeurs manquantes ou constantes en utilisant la méthode value_counts() de Pandas
+        for column in self.df_filtered.columns:
+            print(f"\n--- {column} ---")
+            print(self.df_filtered[column].value_counts(normalize=True, dropna=False) * 100)
 
-# * Mettre en evidence les différences entre les immeubles mono et multi-usages
-df["PropertyActivityNumber"] = df[["SecondLargestPropertyUseType", "ThirdLargestPropertyUseType"]].notna().any(axis=1)
-df["PropertyActivityNumber"] = df["PropertyActivityNumber"].map({True: "Multi-activity", False: "Mono-activity"})
+        print(f"\n---**** {self.df_filtered["LargestPropertyUseType"].value_counts(normalize=True, dropna=False) * 100} ---")
+
+        # * Mettre en evidence les différences entre les immeubles mono et multi-usages
+        self.df_filtered["PropertyActivityNumber"] = self.df_filtered[["SecondLargestPropertyUseType", "ThirdLargestPropertyUseType"]].notna().any(axis=1)
+        self.df_filtered["PropertyActivityNumber"] = self.df_filtered["PropertyActivityNumber"].map({True: "Multi-activity", False: "Mono-activity"})
+
+    def first_graph(self):
+
+        # * Utiliser des pairplots et des boxplots pour faire ressortir les outliers ou des batiments avec des valeurs peu cohérentes d'un point de vue métier 
+        output_dir = "plots"
+        os.makedirs(output_dir, exist_ok=True)
+        numeric_columns = self.df_filtered.select_dtypes(include=["float64", "int64"]).columns
+        # Pairplot
+        pairplot_path = os.path.join(output_dir, "pairplot.png")
+        sns.pairplot(self.df_filtered[numeric_columns])
+        plt.savefig(pairplot_path, dpi=300, bbox_inches="tight")
+        plt.close()
+
+        # Boxplot
+        boxplot_path = os.path.join(output_dir, "boxplot.png")
+        plt.figure(figsize=(10, 6))
+        sns.boxplot(data=self.df_filtered[numeric_columns])
+        plt.xticks(rotation=45)
+        plt.savefig(boxplot_path, dpi=300, bbox_inches="tight")
+        plt.close()
+    # Pour vous inspirer, ou comprendre l'esprit recherché dans une analyse exploratoire, vous pouvez consulter ce notebook en ligne : https://www.kaggle.com/code/pmarcelino/comprehensive-data-exploration-with-python. Il ne s'agit pas d'un modèle à suivre à la lettre ni d'un template d'analyses attendues pour ce projet. 
+
+    # # Modélisation 
+
+    # ### Import des modules 
+
+    #Selection
+    from sklearn.model_selection import (
+        train_test_split,
+        GridSearchCV, 
+        cross_validate,
+    )
+    from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error 
+    from sklearn.inspection import permutation_importance
+
+    #Preprocess
+    from sklearn.compose import ColumnTransformer
+    from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+
+    #Modèles
+    from sklearn.dummy import DummyRegressor
+    from sklearn.linear_model import LinearRegression
+    from sklearn.svm import SVR
+    from sklearn.ensemble import RandomForestRegressor
+
+    # ### Feature Engineering
+
+    # A réaliser : Enrichir le jeu de données actuel avec de nouvelles features issues de celles existantes. 
+
+    # En règle générale : On utilise la méthode .apply() de Pandas pour créer une nouvelle colonne à partir d'une colonne existante. N'hésitez pas à regarder les exemples dans les chapitres de cours donnés en ressource
+
+    # In[ ]:
+    def new_features(self):
+
+        self.df_filtered["BuildingAge"] = self.df_filtered.apply(lambda row: 2025 - row["YearBuilt"], axis=1)
+
+        self.df_filtered["ElectricityShare"] = self.df_filtered.apply(
+            lambda row: row["Electricity(kBtu)"] / row["SiteEnergyUse(kBtu)"]
+            if pd.notna(row["SiteEnergyUse(kBtu)"]) and row["SiteEnergyUse(kBtu)"] != 0
+            else None,
+            axis=1
+        )
+
+        self.df_filtered["GasShare"] = self.df_filtered.apply(
+            lambda row: row["NaturalGas(kBtu)"] / row["SiteEnergyUse(kBtu)"]
+            if pd.notna(row["SiteEnergyUse(kBtu)"]) and row["SiteEnergyUse(kBtu)"] != 0
+            else None,
+            axis=1
+        )
+
+        self.df_filtered.to_excel("2016_Building_Energy_V1.xlsx", index=False)
+
+    # CODE FEATURE ENGINEERING
+
+    # ### Préparation des features pour la modélisation
+
+    # A réaliser :
+    # * Si ce n'est pas déjà fait, supprimer toutes les colonnes peu pertinentes pour la modélisation.
+    # * Tracer la distribution de la cible pour vous familiariser avec l'ordre de grandeur. En cas d'outliers, mettez en place une démarche pour les supprimer.
+
+    def target_distribution(self):
+
+        plt.figure(figsize=(6, 4))
+        sns.histplot(self.df_filtered[self.target], bins=40, kde=True)
+        plt.xlabel(self.target)
+        plt.title("Distribution de la cible SiteEUI")
+        plt.savefig("plots/target_distribution.png", dpi=300, bbox_inches="tight")
+        plt.close()
+
+    def detect_outliers_iqr(self, series, k=5):
+        """
+        Détecte outliers par IQR en ignorant NaN et 0 pour le calcul des quantiles.
+        k : multiplicateur IQR (1.5 classique, 3.0 plus strict)
+        Retourne une Series (index -> valeur) des outliers (issus de la série originale).
+        """
+        # Nettoyage pour calcul des quantiles
+        clean = series.dropna()
+        clean = clean[clean != 0]
+        if clean.empty:
+            return pd.Series([], dtype=series.dtype)
+
+        Q1 = clean.quantile(0.25)
+        Q3 = clean.quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - k * IQR
+        upper_bound = Q3 + k * IQR
+
+        # Appliquer les bornes sur la série originale (pour conserver NaN et 0 non marqués)
+        outliers = series[(series < lower_bound) | (series > upper_bound)]
+        return outliers
+    
+    def delete_outliers(self):
+
+        # IQR
+        outliers_iqr = self.detect_outliers_iqr(self.df_filtered["SiteEUI(kBtu/sf)"], k=3)
+
+        # seuil absolu
+        seuil_physique = 400  # valeur réaliste maximale pour tout type de bâtiment
+        outliers_physique = self.df_filtered[self.df_filtered["SiteEUI(kBtu/sf)"] > seuil_physique]
+
+        # fusionner les deux
+        outliers_total = self.df_filtered.loc[outliers_iqr.index.union(outliers_physique.index)]
+
+        # Colonnes de référence à afficher
+        ref_cols = ["PropertyGFATotal", "NumberofBuildings", "LargestPropertyUseType"]
+
+        # Afficher les lignes des outliers
+        print(f"\nLignes contenant les outliers de SiteEUI (total {len(outliers_total)} lignes) :\n")
+
+        for idx in outliers_total.index:
+            row = self.df_filtered.loc[idx]
+            print(f"Ligne {idx}: SiteEUI = {row['SiteEUI(kBtu/sf)']}")
+            for col in ref_cols:
+                print(f"  {col}: {row[col]}")
+            print("-" * 50)
+
+        valid_uses = "Hospital|Care|Laboratory|Data"
+
+        rows_to_drop = outliers_total[
+            ~outliers_total["LargestPropertyUseType"]
+            .fillna("")
+            .str.contains(valid_uses, case=False)
+        ].index
+
+        # Suppression
+        self.df_filtered = self.df_filtered.drop(index=rows_to_drop)
+
+        print(f"{len(rows_to_drop)} lignes supprimées")
+
+    # * Débarrassez-vous des features redondantes en utilisant une matrice de corrélation de Pearson. Pour cela, utiisez la méthode corr() de Pandas, couplé d'un graphique Heatmap de la librairie Seaborn 
+
+    def pearson(self):
+        df_num = self.df_filtered.select_dtypes(include='number')
+        corr_pearson = df_num.corr(method='pearson')
+        corr_spearman = df_num.corr(method='spearman')
+        # figure size
+        plt.figure(figsize=(30, 10))
+        sns.heatmap(corr_pearson, annot=True, cmap="coolwarm", center=0)
+        # change bottom indicator rotation for a better reading
+        plt.xticks(rotation=40, ha="right")
+        plt.title("Pearson")
+        output_dir = "Buildings parameters correlation"
+        os.makedirs(output_dir, exist_ok=True)
+        plt.savefig(f"{output_dir}/Parameters_correlation_corr_heatmaps.png", dpi=500, bbox_inches="tight")
+        self.df_filtered = self.df_filtered.drop(columns=["SiteEUIWN(kBtu/sf)", "SourceEUI(kBtu/sf)", "SourceEUIWN(kBtu/sf)", "Electricity(kBtu)", "SiteEnergyUse(kBtu)", "SiteEnergyUseWN(kBtu)", "YearBuilt"])
 
 
-# * Utiliser des pairplots et des boxplots pour faire ressortir les outliers ou des batiments avec des valeurs peu cohérentes d'un point de vue métier 
-output_dir = "plots"
-os.makedirs(output_dir, exist_ok=True)
-numeric_columns = df.select_dtypes(include=["float64", "int64"]).columns
-# Pairplot
-pairplot_path = os.path.join(output_dir, "pairplot.png")
-sns.pairplot(df[numeric_columns])
-plt.savefig(pairplot_path, dpi=300, bbox_inches="tight")
-plt.close()
+    # * Réalisez différents graphiques pour comprendre le lien entre vos features et la target (boxplots, scatterplots, pairplot si votre nombre de features numériques n'est pas très élevé).
 
-# Boxplot
-boxplot_path = os.path.join(output_dir, "boxplot.png")
-plt.figure(figsize=(10, 6))
-sns.boxplot(data=df[numeric_columns])
-plt.xticks(rotation=45)
-plt.savefig(boxplot_path, dpi=300, bbox_inches="tight")
-plt.close()
-# Pour vous inspirer, ou comprendre l'esprit recherché dans une analyse exploratoire, vous pouvez consulter ce notebook en ligne : https://www.kaggle.com/code/pmarcelino/comprehensive-data-exploration-with-python. Il ne s'agit pas d'un modèle à suivre à la lettre ni d'un template d'analyses attendues pour ce projet. 
+    def pairplot(self, target, df):
+        selected_feature = [
+            self.target,
+            "BuildingAge",
+            "ElectricityShare",
+            "PropertyGFATotal",
+            "NumberofFloors",
+            "ENERGYSTARTScore"
+        ]
 
-# # Modélisation 
-
-# ### Import des modules 
-
-#Selection
-from sklearn.model_selection import (
-    train_test_split,
-    GridSearchCV, 
-    cross_validate,
-)
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error 
-from sklearn.inspection import permutation_importance
-
-#Preprocess
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
-
-#Modèles
-from sklearn.dummy import DummyRegressor
-from sklearn.linear_model import LinearRegression
-from sklearn.svm import SVR
-from sklearn.ensemble import RandomForestRegressor
-
-# ### Feature Engineering
-
-# A réaliser : Enrichir le jeu de données actuel avec de nouvelles features issues de celles existantes. 
-
-# En règle générale : On utilise la méthode .apply() de Pandas pour créer une nouvelle colonne à partir d'une colonne existante. N'hésitez pas à regarder les exemples dans les chapitres de cours donnés en ressource
-
-# In[ ]:
-
-df["BuildingAge"] = df.apply(lambda row: 2025 - row["YearBuilt"], axis=1)
-
-df["ElectricityShare"] = df.apply(
-    lambda row: row["Electricity(kBtu)"] / row["SiteEnergyUse(kBtu)"]
-    if pd.notna(row["SiteEnergyUse(kBtu)"]) and row["SiteEnergyUse(kBtu)"] != 0
-    else None,
-    axis=1
-)
-
-df["GasShare"] = df.apply(
-    lambda row: row["NaturalGas(kBtu)"] / row["SiteEnergyUse(kBtu)"]
-    if pd.notna(row["SiteEnergyUse(kBtu)"]) and row["SiteEnergyUse(kBtu)"] != 0
-    else None,
-    axis=1
-)
-
-df["SteamShare"] = df.apply(
-    lambda row: row["SteamUse(kBtu)"] / row["SiteEnergyUse(kBtu)"]
-    if pd.notna(row["SiteEnergyUse(kBtu)"]) and row["SiteEnergyUse(kBtu)"] != 0
-    else None,
-    axis=1
-)
-
-df.to_excel("2016_Building_Energy_V1.xlsx", index=False)
-
-# CODE FEATURE ENGINEERING
-
-# ### Préparation des features pour la modélisation
-
-# A réaliser :
-# * Si ce n'est pas déjà fait, supprimer toutes les colonnes peu pertinentes pour la modélisation.
-# * Tracer la distribution de la cible pour vous familiariser avec l'ordre de grandeur. En cas d'outliers, mettez en place une démarche pour les supprimer.
-
-def detect_outliers_iqr(series, k=5):
-    """
-    Détecte outliers par IQR en ignorant NaN et 0 pour le calcul des quantiles.
-    k : multiplicateur IQR (1.5 classique, 3.0 plus strict)
-    Retourne une Series (index -> valeur) des outliers (issus de la série originale).
-    """
-    # Nettoyage pour calcul des quantiles
-    clean = series.dropna()
-    clean = clean[clean != 0]
-    if clean.empty:
-        return pd.Series([], dtype=series.dtype)
-
-    Q1 = clean.quantile(0.25)
-    Q3 = clean.quantile(0.75)
-    IQR = Q3 - Q1
-    lower_bound = Q1 - k * IQR
-    upper_bound = Q3 + k * IQR
-
-    # Appliquer les bornes sur la série originale (pour conserver NaN et 0 non marqués)
-    outliers = series[(series < lower_bound) | (series > upper_bound)]
-    return outliers
-
-# IQR
-outliers_iqr = detect_outliers_iqr(df["SiteEUI(kBtu/sf)"], k=3)
-
-# seuil absolu
-seuil_physique = 400  # valeur réaliste maximale pour tout type de bâtiment
-outliers_physique = df[df["SiteEUI(kBtu/sf)"] > seuil_physique]
-
-# fusionner les deux
-outliers_total = df.loc[outliers_iqr.index.union(outliers_physique.index)]
-
-# Colonnes de référence à afficher
-ref_cols = ["PropertyGFATotal", "NumberofBuildings", "LargestPropertyUseType"]
-
-# Afficher les lignes des outliers
-print(f"\nLignes contenant les outliers de SiteEUI (total {len(outliers_total)} lignes) :\n")
-
-for idx in outliers_total.index:
-    row = df.loc[idx]
-    print(f"Ligne {idx}: SiteEUI = {row['SiteEUI(kBtu/sf)']}")
-    for col in ref_cols:
-        print(f"  {col}: {row[col]}")
-    print("-" * 50)
-
-valid_uses = "Hospital|Care|Laboratory|Data"
-
-rows_to_drop = outliers_total[
-    ~outliers_total["LargestPropertyUseType"]
-      .fillna("")
-      .str.contains(valid_uses, case=False)
-].index
-
-# Suppression
-df_cleaned = df.drop(index=rows_to_drop)
-
-print(f"{len(rows_to_drop)} lignes supprimées")
-
+        sns.pairplot(
+            self.df_filtered[selected_features],
+            diag_kind="kde",
+            corner=True
+        )
+        plt.savefig(f"plots/target_pairplot.png", dpi=500, bbox_inches="tight")
 
     
+    def exec_analysis(self):
 
-# * Débarrassez-vous des features redondantes en utilisant une matrice de corrélation de Pearson. Pour cela, utiisez la méthode corr() de Pandas, couplé d'un graphique Heatmap de la librairie Seaborn 
-# * Réalisez différents graphiques pour comprendre le lien entre vos features et la target (boxplots, scatterplots, pairplot si votre nombre de features numériques n'est pas très élevé).
+        self.doc_analysis()
+        self.first_graph()
+        self.new_features()
+        self.target_distribution()
+        self.delete_outliers()
+        self.pearson()
+        self.pairplot()
+
+results = BuildingEnergyStudy()
+results.exec_analysis()
+
+
+
+
+
+
+
 # *  Séparez votre jeu de données en un Pandas DataFrame X (ensemble de feautures) et Pandas Series y (votre target).
 # * Si vous avez des features catégorielles, il faut les encoder pour que votre modèle fonctionne. Les deux méthodes d'encodage à connaitre sont le OneHotEncoder et le LabelEncoder
 
